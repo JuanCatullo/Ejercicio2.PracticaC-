5d6c7e6ffabd54967c077404cd094a3d80cd6c06
Juan <101681675+JuanCatullo@users.noreply.github.com>
2022-05-05 22:11:34 -0300

    Add files via upload

---
+++ Ejercicio2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ejercicio_Programacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Linea;
+            int PG, PP, PE, Total;
+            Console.WriteLine("Ingrese los partidos ganados");
+            Linea = Console.ReadLine();
+            PG = int.Parse(Linea);
+            Console.WriteLine("Ingrese la cantidad de partidos perdidos");
+            Linea = Console.ReadLine();
+            PP = int.Parse (Linea);
+            Console.WriteLine("Ingrese la cantidad de partidos empatados");
+            PE = int.Parse (Linea);
+
+            Total = (PG * 3) + PE;
+
+            Console.WriteLine("El puntaje obtenido en la temporada es de" + Total + "puntos");
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
